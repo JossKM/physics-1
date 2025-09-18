e68d2b324dfc6a03f186ab6e9fcc27b56e83d2d7
JossKM <31990727+JossKM@users.noreply.github.com>
2025-09-18 11:19:15 -0400

    Partial solution to lab exercise 1

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -9,6 +9,7 @@ See documentation here: https://www.raylib.com/, and examples here: https://www.
 #include "raygui.h"
 #include "game.h"
 
+
 const unsigned int TARGET_FPS = 50; //frames/second
 float dt = 1.0f / TARGET_FPS; //seconds/frame
 float time = 0;
@@ -16,6 +17,9 @@ float x = 500;
 float y = 500;
 float frequency = 1;
 float amplitude = 100;
+
+float speed = 100;
+float angle = 30;
 
 //Changes world state
 void update()
@@ -38,8 +42,17 @@ void draw()
 	GuiSliderBar(Rectangle{ 10, 15, 1000, 20 }, "", TextFormat("%.2f", time), &time, 0, 240);
 	DrawText(TextFormat("T: %6.2f", time), GetScreenWidth() - 140, 10, 30, LIGHTGRAY);
 
-	DrawCircle(x, y, 70, RED);
-	DrawCircle(500 + cos(time * frequency) * amplitude, 500 + sin(time * frequency) * amplitude, 70, GREEN);
+
+	GuiSliderBar(Rectangle{ 10, 100, 200, 100 }, "", TextFormat("Speed: %.0f", speed), &speed, -100, 1000);
+	GuiSliderBar(Rectangle{ 10, 200, 200, 100 }, "", TextFormat("Angle: %.0f", angle), &angle, -180, 180);
+
+	/*DrawCircle(x, y, 70, RED);
+	DrawCircle(500 + cos(time * frequency) * amplitude, 500 + sin(time * frequency) * amplitude, 70, GREEN);*/
+
+	Vector2 startPos = {200, GetScreenHeight() - 200};
+	Vector2 velocity = {cos(angle * DEG2RAD) * speed, -sin(angle * DEG2RAD) * speed};
+
+	DrawLineEx(startPos, startPos + velocity, 3, RED);
 
 	EndDrawing();
 
